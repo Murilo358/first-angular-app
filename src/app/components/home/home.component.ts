c26d5dc746cdb4fefdf7dddaa677c8a5c16641ba
Murilo358 <murilobarbosa358@gmail.com>
2024-11-19 20:48:23 -0300

    created footer ngModule

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../footer/footer.component';
+import { FooterModule } from '../footer/footer.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
