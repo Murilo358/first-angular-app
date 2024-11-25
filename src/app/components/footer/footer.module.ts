eb6df079feaadacf901f0e7e13b577082b66fbca
Murilo358 <murilobarbosa358@gmail.com>
2024-11-25 20:46:12 -0300

    testing angular directives

--- src/app/components/footer/footer.module.ts
+++ src/app/components/footer/footer.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
 import { FooterComponent } from "./footer.component";
 
 @NgModule({
     declarations: [FooterComponent],
-    imports: [BrowserModule],
     providers: [],
     bootstrap: [FooterComponent],
     exports: [FooterComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FooterModule } from '../footer/footer.module';
 
 @Component({
@@ -6,8 +6,18 @@ import { FooterModule } from '../footer/footer.module';
   standalone: true,
   imports: [FooterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  name = "Murilo";
+  count = signal(0);
 
-}
+  list = ["teste1", "teste2", "teste3", "teste4"]
+
+  teste(){
+    this.count.update(value => value + 1);
+    this.name = "Oi " + this.count();
+  }
+
+
+} 
